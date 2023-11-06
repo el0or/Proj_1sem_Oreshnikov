cda50013ec331052f77d79475ed14be5f0918df5
el0or <114936804+el0or@users.noreply.github.com>
2023-11-06 16:47:18 +0300

    Update PZ_4.2.py

---
+++ PZ_4/PZ_4.2.py
@@ -0,0 +1,19 @@
+def length(A, B):
+    if A <= B:
+        return 0  # Если A не больше B, то незанятой части нет
+
+    remaining_length = A
+    while remaining_length >= B:
+        remaining_length -= B
+
+    return remaining_length
+
+# Введите значения A и B
+A = int(input("Введите положительное число A: "))
+B = int(input("Введите положительное число B (меньше A): "))
+
+if A > 0 and B > 0 and A > B:
+    result = length(A, B)
+    print(f"Длина незанятой части отрезка A равна {result}")
+else:
+    print("Пожалуйста, введите положительные числа, где A > B.")
