198d006417d8b8cc516c95618b0ac1532a4fecbd
el0or <114936804+el0or@users.noreply.github.com>
2023-11-10 18:42:27 +0300

    Update PZ_4.1.py

--- PZ_4/PZ_4.1.py
+++ PZ_4/PZ_4.1.py
@@ -1,17 +1,17 @@
-def calculate_e_approximation(N):
-    factorial = 1
-    e_approximation = 1.0
+try:
+    def calculate_exp(N):
+        factorial = 1
+        exp = 1.0
 
-    for i in range(1, N + 1):
-        factorial *= i
-        e_approximation += 1.0 / factorial
+        for i in range(1, N + 1):
+            factorial *= i
+            exp += 1.0 / factorial
+        return exp
 
-    return e_approximation
-
-# Введите значение N
-N = int(input("Введите целое число N (>0): "))
-if N > 0:
-    result = calculate_e_approximation(N)
-    print(f"Приближенное значение e при N = {N} равно {result}")
-else:
-    print("N должно быть больше 0.")
+    # Введите значение N
+    N = int(input("Введите целое число N (>0): "))
+    if N > 0:
+        result = calculate_exp(N)
+        print(f"Приближенное значение e при N = {N} равно {result}")
+except ValueError:
+    print("Введите число больше нуля!!")
