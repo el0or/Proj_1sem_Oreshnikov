7d9ca72f9aecf01b6b8cc56b9e0ee44bd5f669d4
el0or <114936804+el0or@users.noreply.github.com>
2023-11-29 14:07:10 +0300

    Update PZ_6.2.py

--- PZ_6/PZ_6.2.py
+++ PZ_6/PZ_6.2.py
@@ -1,4 +1,16 @@
-﻿#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
-; #Warn  ; Enable warnings to assist with detecting common errors.
-SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
-SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+def find_closest_elements(A):
+    min_diff = abs(A[1] - A[0])
+    closest_indexes = [0, 1]
+    
+    for i in range(1, len(A)-1):
+        diff = abs(A[i+1] - A[i])
+        if diff < min_diff:
+            min_diff = diff
+            closest_indexes = [i, i+1]
+    
+    return sorted(closest_indexes)
+
+# Пример использования функции
+A = [5, 8, 10, 3, 7, 12, 6, 9, 2, 4]
+result = find_closest_elements(A)
+print(result)
