96a3f1df6aca47add2641e64e94fba4541ab7376
el0or <114936804+el0or@users.noreply.github.com>
2023-11-29 14:07:48 +0300

    Update PZ_6.3.py

--- PZ_6/PZ_6.3.py
+++ PZ_6/PZ_6.3.py
@@ -1,4 +1,18 @@
-﻿#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
-; #Warn  ; Enable warnings to assist with detecting common errors.
-SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
-SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+def cyclic_shift(A, K):
+    N = len(A)
+    K %= N  # Предотвращаем выход K за пределы длины списка
+
+    # Создаем вспомогательный список B из 4 элементов
+    B = A[N-K:] + A[:N-K]
+
+    # Копируем элементы из списка B в список A
+    for i in range(N):
+        A[i] = B[i]
+
+    return A
+
+# Пример использования функции
+A = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+K = 2
+result = cyclic_shift(A, K)
+print(result)
