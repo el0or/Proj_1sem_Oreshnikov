bc83b1ed8b0c87132428288a4e82b636cf51edea
el0or <114936804+el0or@users.noreply.github.com>
2023-12-05 17:19:05 +0300

    Update PZ_6.2.py

--- PZ_6/PZ_6.2.py
+++ PZ_6/PZ_6.2.py
@@ -1,16 +1,14 @@
-def find_closest_elements(A):
-    min_diff = abs(A[1] - A[0])
-    closest_indexes = [0, 1]
-    
-    for i in range(1, len(A)-1):
-        diff = abs(A[i+1] - A[i])
-        if diff < min_diff:
-            min_diff = diff
-            closest_indexes = [i, i+1]
-    
-    return sorted(closest_indexes)
+#Дан целочисленный список размера N, содержащий ровно два одинаковых элемента.
+#Найти номера одинаковых элементов и вывести эти номера в порядке возрастания.
 
-# Пример использования функции
-A = [5, 8, 10, 3, 7, 12, 6, 9, 2, 4]
-result = find_closest_elements(A)
+def find_duplicate_indices(nums):
+    dd = {}
+    for i, num in enumerate(nums):
+        if num in dd:
+            return (dd[num], i)
+        else:
+            dd[num] = i
+
+my_list = [3, 1, 5, 2, 7, 5]
+result = find_duplicate_indices(my_list)
 print(result)
