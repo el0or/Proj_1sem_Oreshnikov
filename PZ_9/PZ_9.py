f867c847a28f358286cf1b5b2673261fccd0980b
el0or <114936804+el0or@users.noreply.github.com>
2024-01-20 19:33:09 +0300

    Update PZ_9.py

--- PZ_9/PZ_9.py
+++ PZ_9/PZ_9.py
@@ -1,1 +1,5 @@
+persons = {"Андрей": 32, "Виктор": 29, "Максим": 18, "Елена": 25, "Ольга": 30, "Иван": 27}
 
+average_age = sum(persons.values()) / len(persons)
+
+print("Средний возраст:", average_age)
