d70a6699ccb70694c60e448e5f75af87d50df2b6
VacheslavAksenovIS-25 <aksenovvaceslav2@gmail.com>
2024-06-01 15:44:01 +0300

    PZ_13.2 update

--- PZ_13/PZ_13.2.py
+++ PZ_13/PZ_13.2.py
@@ -1,12 +1,9 @@
 #В матрице найти сумму элементов первых двух строк.
 from functools import reduce
-
+import random
 # Создание матрицы
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+n = 3
+matrix = [[random.randint(1, 20) for _ in range(n)] for _ in range(n)]
 
 # Нахождение суммы элементов первых двух строк матрицы
 sum_first_two_rows = reduce(lambda acc, row: acc + sum(row), matrix[:2], 0)
